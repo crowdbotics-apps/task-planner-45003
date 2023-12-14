ce8a19b7e5f7d364544ccf1a3c29f0731bc895ff
Crowdbotics <team@crowdbotics.com>
2023-12-14 06:45:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "task-planner-45003",
+  url: "https://task-planner-45003.botics.co",
+  api: "https://task-planner-45003.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
